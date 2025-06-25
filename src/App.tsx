161551671031c6e850342a43e2524eb562e5991f
Soham Chandrashekhar Kalse <skalse@dons.usfca.edu>
2025-06-25 12:32:07 -0700

    Added navbar, routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import './App.css'
-
+import Home from './components/Home'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import CheckAuth from './components/CheckAuth'
+import About from './components/About.tsx'
+import Contact from './components/Contact.tsx'
 function App() {
 
   return (
     <>
-      <div>Hello World</div>
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route
+        path='/' 
+        element={<Home />}>
+      </Route>
+
+      <Route path='/about' element={<About />}>
+      </Route>
+      <Route path='/contact' element={<Contact />}>
+      </Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
+import { Paths } from '../commons/paths.ts'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <ul>
+        <NavLink to={Paths.HomePage}><li>Home</li></NavLink>
+        <NavLink to={Paths.AboutPage}><li>About</li></NavLink>
+        <NavLink to={Paths.ContactPage}><li>Contact</li></NavLink>
+      </ul>
+    </nav>
   )
 }
 
